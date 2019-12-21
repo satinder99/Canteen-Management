4de20004d03eb01f26206c69f8c0b297f1a6deaf
satinder99 <kainth00013@gmail.com>
2019-12-21 10:39:43 +0530

    css implemented

--- Login/welcome.php
+++ Login/welcome.php
@@ -27,12 +27,12 @@ header("location:../Breakfast/mybreakfast.php");
 	<head> 
 		<meta charset="utf-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1">
-                <link rel="stylesheet" href="../../include/bootstrap.min.css">
+                <link rel="stylesheet" href="../include/bootstrap.min.css">
              <!--   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-               --> <script src="../../include/bootstrap.min.js"></script>
+               --> <script src="../include/bootstrap.min.js"></script>
                 
-                <link rel='stylesheet' href='../../include/home.css'>
+                <link rel='stylesheet' href='../include/home.css'>
 		<style>
 			#left
 			{
--- Lunch/mylunch.php
+++ Lunch/mylunch.php
@@ -13,9 +13,9 @@ if($_SESSION['user']=='')
                 <link rel="stylesheet" href="../include/bootstrap.min.css">
                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
              <!--   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-               --> <script src="../../include/bootstrap.min.js"></script>
+               --> <script src="../include/bootstrap.min.js"></script>
                 
-                <link rel='stylesheet' href='../../include/home.css'>
+                <link rel='stylesheet' href='../include/home.css'>
                 <style>
                         #left
                         {
